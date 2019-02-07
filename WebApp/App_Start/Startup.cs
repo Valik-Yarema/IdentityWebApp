7160dff12984fc400159cc3cf486f706d07a9296
Valik-Yarema <34391973+Valik-Yarema@users.noreply.github.com>
2019-02-07 13:23:58 +0200

    compleate metanit

---
+++ WebApp/App_Start/Startup.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApp.BLL.Interfaces;
+using WebApp.BLL.Services;
+
+[assembly: OwinStartup(typeof(WebApp.App_Start.Startup))]
+namespace WebApp.App_Start
+{
+    public class Startup
+    {
+        IServiceCreator serviceCreator = new ServiceCreator();
+        public void Configuration(IAppBuilder app)
+        {
+            app.CreatePerOwinContext<IUserService>(CreateUserService);
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                LoginPath = new PathString("/Account/Login"),
+            });
+        }
+
+        private IUserService CreateUserService()
+        {
+            return serviceCreator.CreateUserService("DefaultConnection");
+        }
+    }
+}
